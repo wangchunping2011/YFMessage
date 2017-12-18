955e0a186d065a31eb8e78636e66a5a4450d6671
‘Dandre’ <mkshow@126.com>
2017-12-18 14:14:04 +0800

    修复alert时不显示的bug

--- YFMessage/YFMessage.m
+++ YFMessage/YFMessage.m
@@ -149,7 +149,7 @@ static YFMessage *yfMsg = nil;
     for (UIAlertAction *action in actions) {
         [alert addAction:action];
     }
-    [[UIApplication sharedApplication].delegate.window.rootViewController presentViewController:alert animated:YES completion:nil];
+    [[self getCurrentVC] presentViewController:alert animated:YES completion:nil];
     return alert;
 }
 
@@ -286,6 +286,45 @@ static YFMessage *yfMsg = nil;
         UILabel *message = subView5.subviews[1];
         message.textAlignment = textAlign;
     }
+}
+
+//获取当前屏幕显示的viewcontroller
+- (UIViewController *)getCurrentVC
+{
+    UIViewController *rootViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    
+    UIViewController *currentVC = [self getCurrentVCFrom:rootViewController];
+    
+    return currentVC;
+}
+
+- (UIViewController *)getCurrentVCFrom:(UIViewController *)rootVC
+{
+    UIViewController *currentVC;
+    
+    if ([rootVC presentedViewController]) {
+        // 视图是被presented出来的
+        
+        rootVC = [rootVC presentedViewController];
+    }
+    
+    if ([rootVC isKindOfClass:[UITabBarController class]]) {
+        // 根视图为UITabBarController
+        
+        currentVC = [self getCurrentVCFrom:[(UITabBarController *)rootVC selectedViewController]];
+        
+    } else if ([rootVC isKindOfClass:[UINavigationController class]]){
+        // 根视图为UINavigationController
+        
+        currentVC = [self getCurrentVCFrom:[(UINavigationController *)rootVC visibleViewController]];
+        
+    } else {
+        // 根视图为非导航类
+        
+        currentVC = rootVC;
+    }
+    
+    return currentVC;
 }
 
 @end
